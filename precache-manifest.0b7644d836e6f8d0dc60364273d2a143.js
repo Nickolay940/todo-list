6209596af33ca679cbf96bc6f6619d065d7ebe4a
Nickolay940 <58544772+Nickolay940@users.noreply.github.com>
2020-08-24 17:02:40 +0500

    deploy

---
+++ precache-manifest.0b7644d836e6f8d0dc60364273d2a143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7c9756610537c965af",
+    "url": "/todo-list/css/app.60ed2852.css"
+  },
+  {
+    "revision": "a86dfbcf9c17fe23a4f3973462e0221a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0d7c9756610537c965af",
+    "url": "/todo-list/js/app.6c4f1359.js"
+  },
+  {
+    "revision": "8dd63933601a33041ae9",
+    "url": "/todo-list/js/chunk-vendors.1eaaf0ae.js"
+  },
+  {
+    "revision": "33f7fa286bf61f74c8793938f3d7d99b",
+    "url": "/todo-list/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo-list/robots.txt"
+  }
+]);
